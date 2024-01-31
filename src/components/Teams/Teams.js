10f4cfe0020d60607039064b9589222ca7086b40
Chandan S <chandyshek@gmail.com>
2024-02-01 00:40:20 +0530

    retrieving data for neprofile

--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -1,25 +1,50 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 import isLoggedIn from "../../auth/isLoggedIn";
 import { Navigate, Outlet } from "react-router-dom";
 import axios from "axios";
+import Cookies from "universal-cookie";
 function Teams() {
-    const navigate=useNavigate()
-    async function fetchData() {
-        let response = await axios.get('http://localhost:5000/teams/read');
-        if(response.status===401){
-            navigate("/teamreg")
-        }
-        else if(response.status===201){
-            navigate("/teamprofile")
-        }
+    const cookies = new Cookies();
+    const navigate = useNavigate()
+    async function fetchData(getting) {
+        var config = {
+            headers: { "Accept": "/", "token": `${ getting }`
     }
-     useEffect(()=>{
-       fetchData()
-   },[])
-  return <div>
+}
+console.log(config);
+let response = await axios.get('http://localhost:5000/teams/read', config);
+if (response.status === 200) {
+    if (response.data.success) {
+        navigate("/user/teamprofile")
+    }
+    else {
+        navigate("/user/teamreg")
+    }
+}
+    }
+async function retrieve() {
+    let getting = await cookies.get("jwt_authorization")
+    cookies.get({
+        name: "jwt_authorization",
+    })
+    if (getting) {
+        console.log(getting);
+        fetchData(getting);
+        console.log("Redirected");
+    }
+    else {
+        console.log("error");
+    }
+}
+useEffect(() => {
+    retrieve()
+}, [])
+
+
+return <div>
     {isLoggedIn() ? <Outlet /> : <Navigate to="/login" />}
-    </div>;
+</div>;
 }
 
-export default Teams
+export default Teams;
